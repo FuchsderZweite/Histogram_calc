ac03ca07bdc59283c80763b7193375a8b6e6e564
email.sergej@web.de <sergej24071989>
2021-01-15 16:29:18 +0100

    added "path to local image"

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+path_to_local_img = r'/Users/sergej/Desktop/Ablage/Neuer Ordner/IMG_9998.JPG'
+#path_to_local_img = 'C:\Users\Sergej\Desktop\abc1.tif'
 
-
-
-
-img = cv2.imread(r'C:\Users\Sergej\Desktop\abc1.tif', - 1)
+img = cv2.imread(path_to_local_img, - 1)
 max = np.amax(img)
-
 
 hist = cv2.calcHist([img],[0],None,[256],[0.24,0.3])
 print(bin(max))
 plt.hist(img.ravel(), 256, [0.24,0.3])
 plt.show()
 
-
-
 def find_min_max(img):
     min = np.amin(img)
     max = np.amax(img)
     return min, max
+
+def find_img_type(img):
+    pass
