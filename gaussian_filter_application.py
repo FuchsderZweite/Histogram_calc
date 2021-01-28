99393fc476e664412f5d754fbc4c2e5079ea6b97
Sergej <email.sergej@web.de>
2021-01-28 14:03:11 +0100

    added static polynom fit

--- gaussian_filter_application.py
+++ gaussian_filter_application.py
@@ -1,6 +1,6 @@
 from scipy.ndimage import gaussian_filter, median_filter
 from scipy.optimize import curve_fit
-from scipy.signal import argrelextrema
+from scipy.signal import find_peaks
 import numpy as np
 import cv2 as cv
 import os
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 import time
 
 
-#working_dir = r'C:\Users\Sergej\Desktop\Sample_DATA_Jakob_SMALL\2020-08-13-zbl-GFK_Impact_gebogen-40kV-15W-150ms-10mit-nofilt_tifs' # prep dataset (20 images)
+working_dir = r'C:\Users\Sergej\Desktop\Sample_DATA_Jakob_SMALL\2020-08-13-zbl-GFK_Impact_gebogen-40kV-15W-150ms-10mit-nofilt_tifs' # prep dataset (33 images)
 #working_dir = r'C:\Users\Sergej\Desktop\Sample_DATA_Jakob\2020-08-13-zbl-GFK_Impact_gebogen-40kV-15W-150ms-10mit-nofilt_tifs' # full dataset (2.4k images)
-working_dir =r'C:\Users\Rechenfuchs\Documents\GitHub\dummy_data_for_hist_calc'
+#working_dir =r'C:\Users\Rechenfuchs\Documents\GitHub\dummy_data_for_hist_calc'
 
 sigma = 10
 step = 1
@@ -18,11 +18,16 @@ median_size=5
 parameter_set = (working_dir, sigma, median_size)
 
 
-def plot_data(plot_values, step):
-    ydata, xdata = plot_values
-    x = xdata[::step]
-    y = ydata[::step]
-    plt.plot(x, y, '-ob', alpha=0.7, label='sigma ={} \n step = {}'.format(sigma, step))
+def plot_data(plot_values, fit_values, step):
+    y_data, x_data = plot_values
+    poly3, poly4, poly5, poly6 = fit_values
+    x = x_data[::step]
+    y = y_data[::step]
+    plt.scatter(x, y, 'ob', alpha=0.7, label='sigma ={} \n median size = {} \n step = {}'.format(sigma, median_size, step))
+    plt.plot(x, fit_values[0], '-r', alpha=0.7, label='sigma ={} \n median size = {} \n step = {}'.format(sigma, median_size, step))
+    plt.plot(x, fit_values[1], '-r', alpha=0.7, label='sigma ={} \n median size = {} \n step = {}'.format(sigma, median_size, step))
+    plt.plot(x, fit_values[2], '-r', alpha=0.7, label='sigma ={} \n median size = {} \n step = {}'.format(sigma, median_size, step))
+    plt.plot(x, fit_values[3], '-r', alpha=0.7, label='sigma ={} \n median size = {} \n step = {}'.format(sigma, median_size, step))
     plt.title(r'Minimum(Intensity) as a function of the projection')
     ax = plt.gca()
     [l.set_visible(False) for (i, l) in enumerate(ax.xaxis.get_ticklabels()) if i % step != 0]
@@ -34,55 +39,70 @@ def plot_data(plot_values, step):
     return 0
 
 
-def fit_sin(x_values, A, B, C, D):
-    return A*np.sin(C*x_values + D) + B
+def get_peaks(plot_values):
+    y_data, x_data = plot_values
+    y = y_data[::step]
+    x_data = np.arange(y_data.size)
+    x = x_data[::step]
+    #F = (np.amax(y) - np.amin(y))/ 2
+    #best_guess = [1,1,1,1,1, F]
+    return x,y
 
 
-def polynom_fit(x, A, B, C, D, E, F):
-    return A*x**5 + B*x**4 + C*x**3 + D*x**2 + E*x + F
+def polynom_fit(peak_data):
+    x, y = peak_data
+    range = np.arange(3,15)
+    fit3 = []
+    fit4 = []
+    fit5 = []
+    fit6 = []
+    try:
+        fit3 = np.polyfit(x, y, 3)
+    except:
+        print('No fit could be found for degree=3.')
+    try:
+        fit4 = np.polyfit(x, y, 4)
+    except:
+        print('No fit could be found for degree=4.')
+    try:
+        fit5 = np.polyfit(x, y, 5)
+    except:
+        print('No fit could be found for degree=5.')
+    try:
+        fit6 = np.polyfit(x, y, 6)
+    except:
+        print('No fit could be found for degree=6.')
+    return fit3, fit4, fit5, fit6
 
 
 
 
 def add_filter(dir, sigma, median_size):
-    i = 0
     list_min_val = []
+    list_min_val_arg = []
     list_filenames = []
+    print(
+        'The source directory contains {} files.\n'
+        'The process will take approx. {} sec.'.format(len(os.listdir(dir)), round(1.4 * len(os.listdir(dir)))))
     try:
         for filename in os.listdir(dir):
-            num1 = i+1
-            num2 = len(os.listdir(dir))
-            progress = 100*(num1/num2)
             raw_img = cv.imread(os.path.join(dir, filename), 2)
             median_filtered_img = median_filter(raw_img, median_size)                   # 1.1 sec
             gaussian_filtered_img = gaussian_filter(median_filtered_img, sigma=sigma)   # 0.3 sec
             list_min_val.append(np.amin(gaussian_filtered_img))
             list_filenames.append(filename)
-            if (num1 % 2 == 0):
-                print('{}% done.'.format(round(progress)))
-            else:
-                pass
-            i += 1
+
     except FileNotFoundError :
         print('File not fount! Check the working directory.')
     arr_min_val = np.array(list_min_val)
+    arr_min_val_arg = np.array(list_min_val_arg)
     arr_filename = np.array(list_filenames)
-    return arr_min_val, arr_filename    # brauche (zum fitten) nicht die Dateinamen sondern die y-Werte!
-
-
+    return arr_min_val, arr_filename
 
 plot_values = add_filter(*parameter_set)
+peaks = get_peaks(plot_values)
+fit_values = polynom_fit(peaks)
+#poly3, poly4, poly5, poly6 = fit_values
 
-
-
-ydata, xdata = plot_values
-x = xdata[::step]
-y = ydata[::step]
-
-best_guess = [1, 1, 1, 1, 1, 1]
-popt, covt = curve_fit(polynom_fit, xdata=x, ydata=y, p0=best_guess)
-A, B, mu1, mu2, sig1, sig2 = popt
-
-
-#plot_data(plot_values, step)
+plot_data(plot_values, fit_values, step)
 
--- test_file.py
+++ test_file.py
@@ -11,12 +11,6 @@ working_dir =r'C:\Users\Rechenfuchs\Documents\GitHub\dummy_data_for_hist_calc'
 bins = 100
 
 
-def find_peak(bins):
-    sorted_bins = bins.sort()
-    p1 = sorted_bins[-1]
-    p2 = sorted_bins[-2]
-    # read the ys values and find the highest two bins -> best guess values.
-    return
 
 
 def write_to_file(x_val, file_name):
@@ -29,36 +23,66 @@ def gaussian(x, A, B, mu1, mu2, sig1, sig2):
     return A * np.exp(-np.power(x - mu1, 2.) / (2 * np.power(sig1, 2.))) + B * np.exp(
         -np.power(x - mu2, 2.) / (2 * np.power(sig2, 2.)))
 
-def polynom_fit(x, A, B, C, D, E, F):
-    return A*x**5 + B*x**4 +C*x**3 +D*x**2 +E*x + F
+
+
+
+a = np.arange(1,17)
+x = np.array(a)
+y = np.array([12,8,5,5,6,6,7,8,8,7,6,7,9,6,4,1])
+
+def poly3(x, params):
+    A, B, C, D = params
+    return A*x**3 + B*x**2 + C*x + D
+
+def poly4(x, params):
+    A, B, C, D, E = params
+    return A*x**4 + B*x**3 + C*x**2 + D*x + E
+
+def poly7(x, params):
+    A, B, C, D, E, F, G, H = params
+    return A*x**7 + B*x**6 + C*x**5 + D*x**4 + E*x**3 + F*x**2 + G*x + H
+
+params3 = np.polyfit(x, y, 3)
+params4 = np.polyfit(x, y, 4)
+params7 = np.polyfit(x, y, 7)
+
+
+plt.scatter(x, y, marker='o', alpha=0.7, label=r'data')
+plt.plot(x, poly3(x, params3), color='red', linestyle='-', linewidth=2.5, alpha=0.7, label=r'$x^{3}$')
+plt.plot(x, poly4(x, params4), color='green', linestyle='-', linewidth=2.5, alpha=0.7, label=r'$x^{4}$')
+plt.plot(x, poly7(x, params7), color='purple', linestyle='-', linewidth=2.5, alpha=0.7, label=r'$x^{7}$')
+plt.title('Fit comparison')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
 
 
 
 
 
-'''
-def calc_histogram(dir):
-    peaks = []
-    for filename in os.listdir(dir):
+#def calc_histogram(dir):
+#    peaks = []
+#    for filename in os.listdir(dir):
         #img = cv.imread(r'C:\Users\Sergej\Desktop\abc2.tif', 2) # hier muss der Histogram-Rechenr rein
-        img = cv.imread(os.path.join(dir, filename), 2) # hier ist ein Problem. Die Bilder werden als None deklariert -> fehler beim Einlesen
-        ys, xs, patches = plt.hist(img.ravel(), bins=bins)
-        bin_center = np.array([0.5 * (xs[i] + xs[i + 1]) for i in range(len(xs) - 1)])
-        bin_width = xs[1] - xs[0]
+#        img = cv.imread(os.path.join(dir, filename), 2) # hier ist ein Problem. Die Bilder werden als None deklariert -> fehler beim Einlesen
+#        ys, xs, patches = plt.hist(img.ravel(), bins=bins)
+#        bin_center = np.array([0.5 * (xs[i] + xs[i + 1]) for i in range(len(xs) - 1)])
+#        bin_width = xs[1] - xs[0]
 
-        best_guess = [300000, 150000, 1, 1, 1, 0.1]
-        popt, covt = curve_fit(gaussian, xdata=bin_center, ydata=ys, p0=best_guess)
-        A, B, mu1, mu2, sig1, sig2 = popt
+#        best_guess = [300000, 150000, 1, 1, 1, 0.1]
+#        popt, covt = curve_fit(gaussian, xdata=bin_center, ydata=ys, p0=best_guess)
+#        A, B, mu1, mu2, sig1, sig2 = popt
 
-        x_value_of_peak = find_first_peak(mu1, mu2)
+#        x_value_of_peak = find_first_peak(mu1, mu2)
 
-        peaks = peaks.append(x_value_of_peak)
+#        peaks = peaks.append(x_value_of_peak)
 
         #if img is not None:
         #   images.append(img)
-    return peaks
+#    return peaks
 
-
+'''
 calc_histogram(working_dir)
 
 
@@ -67,16 +91,9 @@ def extract_file_name(dir):
     return head, tail
 
 
-def find_first_peak(mu1, mu2):
-    if mu1 < mu2:
-        return mu1
-    else:
-        return mu2
-
-
 
 # ===== READ IMG ======
-img = cv.imread(r'C:\Users\Sergej\Desktop\abc2.tif', 2)
+
 
 ys, xs, patches = plt.hist(img.ravel(), bins=bins)
 bin_center = np.array([0.5 * (xs[i] + xs[i + 1]) for i in range(len(xs) - 1)])
